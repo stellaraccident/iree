ecda89b8ecbfcbaa34c12bd486281cb027c08946
stanley-nod <stanley@nod-labs.com>
2021-05-16 19:28:52 -0700

    Adding ROCM Backend to Experimental
    Initial pass to integrate ROCm in to IREE so that we can Codegen and run on AMDGPUs. Following steps similar to thomasraoux's CUDA backend. Since ROCm do not have graph or CommandBuffer by default, we implement ROCm's command buffer with Arrays. Tested out and pass most CTS tests except:
    semaphore_submission_test + semaphore_test-> some functionalities not implemented for rocm backend yet
    command_buffer_test -> some quirk with hipErrorNotInitialized during launch of kernel + CommandBufferTest.CopySubBuffer
    
    **-Does depend on harsh-nod's [PR](https://github.com/google/iree/pull/5881)**
    
    In the next patch:
    -Complete semaphore functionality
    -Squash CommandBuffer bugs

---
+++ experimental/rocm/array_command_buffer.h
@@ -0,0 +1,59 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef IREE_HAL_ROCM_ARRAY_COMMAND_BUFFER_H_
+#define IREE_HAL_ROCM_ARRAY_COMMAND_BUFFER_H_
+
+#include "iree/hal/api.h"
+#include "experimental/rocm/context_wrapper.h"
+#include "experimental/rocm/dynamic_symbols.h"
+#include "experimental/rocm/rocm_headers.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+// ROCM Kernel Information Structure
+typedef struct {
+  hipFunction_t func;
+  unsigned int gridDimX;
+  unsigned int gridDimY;
+  unsigned int gridDimZ;
+  unsigned int blockDimX;
+  unsigned int blockDimY;
+  unsigned int blockDimZ;
+  void **kernelParams;
+} hip_launch_params;
+
+typedef struct {
+  size_t num_of_kernels;
+  size_t total_size;
+  hip_launch_params kernels[];
+} kernelArrayType;
+
+// Creates a rocm array.
+iree_status_t iree_hal_rocm_array_command_buffer_allocate(iree_hal_rocm_context_wrapper_t *context,
+                                                          iree_hal_command_buffer_mode_t mode,
+                                                          iree_hal_command_category_t command_categories,
+                                                          iree_hal_queue_affinity_t queue_affinity,
+                                                          iree_hal_command_buffer_t **out_command_buffer);
+
+// Returns the array of kernel associated to the command buffer.
+kernelArrayType *iree_hal_rocm_array_command_buffer_exec(const iree_hal_command_buffer_t *command_buffer);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // IREE_HAL_ROCM_ARRAY_COMMAND_BUFFER_H_
